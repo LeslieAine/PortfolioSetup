9151b2facfe70e04a36117a86a807523943aeb30
LeslieAine <aineleslie@gmail.com>
2023-01-03 15:35:08 +0300

    Clean up js file

--- script.js
+++ script.js
@@ -25,22 +25,3 @@ mobileMenuLinks.forEach((mobileMenuLink) => {
   });
 });
 
-// const navBtn = document.querySelector('nav button');
-// const overlay = document.querySelector('.mobile-menu-overlay');
-// const navList = document.querySelectorAll('.overlay-list a');
-// const menuButton = document.querySelector('#menu');
-
-// navBtn.addEventListener('click', () => {
-//   overlay.classList.toggle('mobile-menu-overlay1');
-//   menuButton.classList.toggle('navigate');
-//   menuButton.classList.toggle('overlay-cross-button');
-// });
-
-// navList.forEach((a) => {
-//   a.addEventListener('click', () => {
-//     if (overlay.classList.contains('mobile-menu-overlay1')) {
-//       overlay.classList.value = 'mobile-menu-overlay';
-//       menuButton.classList.value = 'overlay-cross-button';
-//     }
-//   });
-// });
