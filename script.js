fb26f96b4307bd21849311deebaf2f92c3d997bd
LeslieAine <aineleslie@gmail.com>
2023-01-03 08:53:07 +0300

    Added js to interact with overlay

---
+++ script.js
@@ -0,0 +1,25 @@
+const crossButton = document.querySelector('.overlay-cross-button');
+const mobileMenuOverlay = document.querySelector('.mobile-menu-overlay');
+const navHamIcon = document.querySelector('.navigate');
+const mobileMenuLinks = document.querySelectorAll('.list-item > a');
+
+function hideMobileMenu() {
+  requestAnimationFrame(() => {
+    mobileMenuOverlay.style.left = '100%';
+  });
+}
+
+navHamIcon.addEventListener('click', () => {
+  mobileMenuOverlay.style.left = '0%';
+  mobileMenuOverlay.style.display = 'flex';
+});
+
+crossButton.addEventListener('click', () => {
+  hideMobileMenu();
+});
+
+mobileMenuLinks.forEach((mobileMenuLink) => {
+  mobileMenuLink.addEventListener('click', () => {
+    hideMobileMenu();
+  });
+});
