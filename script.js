cdee859c1984a7e8fbd0e1871a784cca080029c9
LeslieAine <aineleslie@gmail.com>
2023-01-03 19:18:49 +0300

    Fixed hamburger issue from review

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ const mobileMenuOverlay = document.querySelector('.mobile-menu-overlay');
 const navHamIcon = document.querySelector('.navigate');
 const mobileMenuLinks = document.querySelectorAll('.list-item > a');
 
-function hideMobileMenu() {
-  requestAnimationFrame(() => {
-    mobileMenuOverlay.style.left = '100%';
-    mobileMenuOverlay.style.display = 'none';
-  });
-}
+// function hideMobileMenu() {
+//     mobileMenuOverlay.style.right = '100%';
+//     mobileMenuOverlay.style.display = 'none';
+// }
 
 navHamIcon.addEventListener('click', () => {
-  mobileMenuOverlay.style.left = '0%';
-  mobileMenuOverlay.style.display = 'flex';
+    mobileMenuOverlay.style.right = '0';
+    mobileMenuOverlay.style.display = 'flex';
 });
 
 crossButton.addEventListener('click', () => {
-  hideMobileMenu();
+    mobileMenuOverlay.style.right = '100%';
+    mobileMenuOverlay.style.display = 'none';
 });
 
 mobileMenuLinks.forEach((mobileMenuLink) => {
   mobileMenuLink.addEventListener('click', () => {
-    hideMobileMenu();
+    mobileMenuOverlay.style.right = '100%';
+    mobileMenuOverlay.style.display = 'none';
   });
 });
 
