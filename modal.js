030afdbb0f70e5df3cf960d8ca88edded346fbad
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-01-05 14:26:23 +0530

    dynamic skeleton

--- modal.js
+++ modal.js
@@ -1,103 +1,216 @@
 const card1 = {
-    title: 'Keeping track of hundreds  of components website',
-    techList: [
-        'html',
-        'Bootstrap',
-        'Ruby on rails'
-    ],
-    description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+  title1 : 'professional Art Printing Data leslie',
+  title: 'Keeping track of hundreds  of components website',
+  techList: [
+    'html',
+    'Bootstrap',
+    'Ruby on rails'
+  ],
+  description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
       standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
       make a type specimen book. It has survived not only five centuries, but also the leap into electronic
       typesetting, remaining essent'`,
-    link: [
-      'https://LeslieAine.github.io/PortfolioSetup/',
-      'https://github.com/LeslieAine/',
-    ],
-    image: 'images/card1.png',
-  };
-  
-  const card2 = {
-    title: 'Multi Post Stories',
-    techList: [
-        'html',
-        'Bootstrap',
-        'Ruby on rails'
-    ],
-    description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+  link: [
+    'https://LeslieAine.github.io/PortfolioSetup/',
+    'https://github.com/LeslieAine/',
+  ],
+  lilDescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard`,
+  image: 'images/card1.png',
+};
+
+const card2 = {
+  title1 : 'professional Art Printing Data pathak',
+  title: 'Multi Post Stories',
+  techList: [
+    'html',
+    'Bootstrap',
+    'Ruby on rails'
+  ],
+  description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
       standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
       make a type specimen book. It has survived not only five centuries, but also the leap into electronic
       typesetting, remaining essent'`,
-    link: [
-        'https://LeslieAine.github.io/PortfolioSetup/',
-        'https://github.com/LeslieAine/',
-    ],
-    image: 'images/card2.png',
-  };
-  
-  const card3 = {
-    title: 'Multi Post Stories',
-    techList: [
-        'html',
-        'Bootstrap',
-        'Ruby on rails'
-    ],
-    description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+  link: [
+    'https://LeslieAine.github.io/PortfolioSetup/',
+    'https://github.com/LeslieAine/',
+  ],
+  lilDescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard`,
+  image: 'images/card2.png',
+};
+
+const card3 = {
+  title1 : 'professional Art Printing Data anie ',
+  title: 'Multi Post Stories',
+  techList: [
+    'html',
+    'Bootstrap',
+    'Ruby on rails'
+  ],
+  description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
       standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
       make a type specimen book. It has survived not only five centuries, but also the leap into electronic
       typesetting, remaining essent'`,
-    link: [
-        'https://LeslieAine.github.io/PortfolioSetup/',
-        'https://github.com/LeslieAine/',
-    ],
-    image: 'images/card2.png',
-  };
-  
-  const card4 = {
-    title: 'Multi Post Stories',
-    techList: [
-        'html',
-        'Bootstrap',
-        'Ruby on rails'
-    ],
-    description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+  link: [
+    'https://LeslieAine.github.io/PortfolioSetup/',
+    'https://github.com/LeslieAine/',
+  ],
+  lilDescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard`,
+  image: 'images/card2.png',
+};
+
+const card4 = {
+  title1 : 'professional Art Printing Data adarsh',
+  title: 'Multi Post Stories',
+  techList: [
+    'html',
+    'Bootstrap',
+    'Ruby on rails'
+  ],
+  description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
       standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
       make a type specimen book. It has survived not only five centuries, but also the leap into electronic
       typesetting, remaining essent'`,
-    link: [
-        'https://LeslieAine.github.io/PortfolioSetup/',
-        'https://github.com/LeslieAine/',
-    ],
-    image: 'images/card2.png',
-  };
-  
-  const projects = [card1, card2, card3, card4];
-  function modal(number) {
-    const card = document.querySelector('#card');
-    card.classList.toggle('invisible');
-    const title = document.querySelector('#card-title');
-    
-    const img = document.querySelector('.card-image');
-    const desc = document.querySelector('#card-description');
-    const techList = document.querySelector('#tech-list');
-    techList.innerHTML = '';
-    const buttonsrc = document.querySelector('#card-button-source');
-    const buttonlive = document.querySelector('#card-button-live');
-    title.innerHTML = projects[number].title;
-    
-    img.setAttribute('src', projects[number].image);
-    for (let i = 0; i < projects[number].techList.length; i += 1) {
-      const li = document.createElement('li');
-      li.textContent = projects[number].techList[i];
-      techList.appendChild(li);
-    }
-    desc.innerHTML = projects[number].description;
-    buttonsrc.setAttribute('onclick', `window.open("${projects[number].link[1]}", "_blank");`);
-    buttonlive.setAttribute('onclick', `window.open("${projects[number].link[0]}", "_blank");`);
+  link: [
+    'https://LeslieAine.github.io/PortfolioSetup/',
+    'https://github.com/LeslieAine/',
+  ],
+  lilDescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard`,
+  image: 'images/card2.png',
+};
+
+const card5 = {
+  title1 : 'professional  Printing Data',
+  title: 'Multi Post Stories',
+  techList: [
+    'html',
+    'Bootstrap',
+    'Ruby on rails'
+  ],
+  description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essent'`,
+  link: [
+    'https://LeslieAine.github.io/PortfolioSetup/',
+    'https://github.com/LeslieAine/',
+  ],
+  lilDescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard`,
+  image: 'images/card2.png',
+};
+
+const card6 = {
+  title1 : 'Professional Art Printing ',
+  title: 'Multi Post Stories',
+  techList: [
+    'html',
+    'Bootstrap',
+    'Ruby on rails'
+  ],
+  description: `'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+      standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+      typesetting, remaining essent'`,
+  link: [
+    'https://LeslieAine.github.io/PortfolioSetup/',
+    'https://github.com/LeslieAine/',
+  ],
+  lilDescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard`,
+  image: 'images/card2.png',
+};
+
+const projects = [card1, card2, card3, card4, card5, card6];
+
+const bo = document.querySelector('body')
+const article = document.createElement('article')
+article.className = 'card-container invisible';
+article.setAttribute('id', 'card');
+article.innerHTML = `
+    <div class="card">
+      <i id="card-cross" class="fas fa-times card-cross" onclick="toggle()"></i>
+      <h3 id="card-title">See Project header</h3>
+      <ul id="tech-list" class="tech">
+      </ul>
+      <div class="popupFooter">
+        <img src="images/card1" class="card-image" alt="project screenshot">
+        <iframe src="" frameborder="0" class="card-iframe invisible"></iframe>
+        
+
+        <div class="functionality">
+          <p id="card-description">
+          </p>
+          <div class="button-container">
+            <button type="button" id="card-button-live" class="card-button">
+                See live
+              <i class="fas fa-external-link-alt"></i>
+            </button>
+            <button type="button" id="card-button-source" class="card-button">
+              See source
+              <i class="fab fa-github"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+    </div>
+`;
+bo.appendChild(article);
+
+for (let i = 0; i < projects.length; i += 1) {
+  const proCard = document.querySelector('#proCard')
+  const prof = document.createElement('section')
+  prof.className = 'Professional'
+  prof.innerHTML = `
+      <h3 class="heading5">${projects[i].title1}</h3>
+      <p class="art-words">
+        ${projects[i].lilDescription}
+      </p>
+      <a class="bgimage2"></a>
+      <ul class="f-item">
+        <li>
+          <a class="icon2" href="#"><img src="images/html1.png" alt="html" /></a>
+        </li>
+        <li>
+          <a class="icon2" href="#"><img src="images/bootstrap1.png" alt="bootstrap" /></a>
+        </li>
+        <li>
+          <a class="icon2" href="#"><img src="images/ruby1.png" alt="ruby" /></a>
+        </li>
+      </ul>
+
+      <div class=buttonlink onclick="modal(1)">
+        <button class="button2">See Project</button>
+      </div>
+  `;
+  proCard.appendChild(prof);
+}
+
+
+function modal(number) {
+  const card = document.querySelector('#card');
+  card.classList.toggle('invisible');
+  const title = document.querySelector('#card-title');
+
+  const img = document.querySelector('.card-image');
+  const desc = document.querySelector('#card-description');
+  const techList = document.querySelector('#tech-list');
+  techList.innerHTML = '';
+  const buttonsrc = document.querySelector('#card-button-source');
+  const buttonlive = document.querySelector('#card-button-live');
+  title.innerHTML = projects[number].title;
+
+  img.setAttribute('src', projects[number].image);
+  for (let i = 0; i < projects[number].techList.length; i += 1) {
+    const li = document.createElement('li');
+    li.textContent = projects[number].techList[i];
+    techList.appendChild(li);
   }
-  function toggle() {
-    const card = document.querySelector('#card');
-    card.classList.toggle('invisible');
-  }
-  toggle();
-  modal();
-  
+  desc.innerHTML = projects[number].description;
+  buttonsrc.setAttribute('onclick', `window.open("${projects[number].link[1]}", "_blank");`);
+  buttonlive.setAttribute('onclick', `window.open("${projects[number].link[0]}", "_blank");`);
+}
+function toggle() {
+  const card = document.querySelector('#card');
+  card.classList.toggle('invisible');
+}
+toggle();
+modal();
