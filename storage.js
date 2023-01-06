e1a5c1f7896edb9c8f5c7705af28a7215bdb8d52
LeslieAine <aineleslie@gmail.com>
2023-01-06 10:30:39 +0300

    Added local storage to html for form

---
+++ storage.js
@@ -0,0 +1,29 @@
+const userName = document.getElementById('fname');
+const userEmail = document.getElementById('email');
+const userComment = document.getElementById('textarea');
+const contactForm = document.getElementById('contact-form');
+
+function storeForm() {
+  const myStorage = {
+    userName: userName.value,
+    userEmail: userEmail.value,
+    userComment: userComment.value,
+  };
+  localStorage.setItem('storage', JSON.stringify(myStorage));
+}
+
+userName.addEventListener('change', storeForm);
+userEmail.addEventListener('change', storeForm);
+userComment.addEventListener('change', storeForm);
+
+function refresh() {
+  if (localStorage.getItem('storage')) {
+    const oldStorage = localStorage.getItem('storage');
+    const newStorage = JSON.parse(oldStorage);
+    contactForm.name.value = newStorage.userName;
+    contactForm.email.value = newStorage.userEmail;
+    contactForm.message.value = newStorage.userComment;
+  }
+}
+
+window.onload = refresh;
